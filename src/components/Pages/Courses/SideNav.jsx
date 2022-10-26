00e2de50132611a20fed8a3c816af9d14427b211
Shamim Sarkar <shamimibas@gmail.com>
2022-10-27 01:17:47 +0600

    Changed favivon and title name

--- src/components/Pages/Courses/SideNav.jsx
+++ src/components/Pages/Courses/SideNav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const SideNav = ({ course }) => {
   return (
     <>
-    <Link to={`/course-details/${course.id}`} className="btn bg-transparent  hover:bg-rose-800 hover:text-white border-0 rounded-none border-r-2 text-right hover:text-2xl trasition ease-in duration-300" rel="noopener noreferrer" href="#">
+    <Link to={`/course-details/${course.id}`} className="btn bg-transparent dark:text-gray-400 hover:bg-rose-800 hover:text-white border-0 rounded-none border-r-2 text-right hover:text-2xl trasition ease-in duration-300 text-slate-500"  href="#">
       {course.name}
     </Link>
     <hr /></>
